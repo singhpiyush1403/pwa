e79f7f49795d5fa382f70604d31f17ade761f28e
PIYUSH SINGH <singhpiyush1403@gmail.com>
2025-06-08 15:35:06 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+if ("serviceWorker" in navigator) {
+  window.addEventListener("load", () => {
+    navigator.serviceWorker
+      .register("/service-worker.js")
+      .then(reg => console.log("✅ Service Worker registered!", reg))
+      .catch(err => console.error("❌ Service Worker registration failed:", err));
+  });
+}
